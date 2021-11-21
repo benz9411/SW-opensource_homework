2f14c6e9e4f6e442e170f0ccec295929a7677d06
benz9411 <benz9411@naver.com>
2021-11-21 23:31:32 +0900

    tmp_bash.sh getopt

---
+++ tmp_bash.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bsh
+# parse command line into arguments
+set -- `getopt a:bc $*`
+# check result of parsing
+if [ $? != 0 ]
+then
+        exit 1
+fi
+while [ $1 != -- ]
+do
+        case $1 in
+        -a)     # set up the -a flag
+                AFLG=1
+                AARG=$2
+                shift;;
+        -b)     # set up the -b flag
+                BFLG=1;;
+        -c)     # set up the -c flag
+                CFLG=1;;
+        esac
+        shift   # next flag
+done
+shift   # skip --
+# now do the work
+.
+.
+.
